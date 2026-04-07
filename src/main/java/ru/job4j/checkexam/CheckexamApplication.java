f0708a05d9c4fcba7b4e10da0f1b1b1fdd5a251c
kir <kirpro95@yandex.ru>
2026-04-07 13:35:28 +0300

    initial commit

---
+++ src/main/java/ru/job4j/checkexam/CheckexamApplication.java
@@ -0,0 +1,13 @@
+package ru.job4j.checkexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckexamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/job4j/checkexam/CheckexamApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.job4j.checkexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckexamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
